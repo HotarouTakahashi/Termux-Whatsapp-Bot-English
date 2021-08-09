d21db80ba11425dfcd599542a5b41b623076e953
Harris <75727795+HotarouTakahashi@users.noreply.github.com>
2021-08-09 20:20:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["919167739828@s.whatsapp.net"] // replace this with your number
+			const ownerNumber = ["9191677*****@s.whatsapp.net"] // replace this with your number
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
